b37bebc0d8ae123d9e3d4e525bc7c560efc221f6
Jack Schwabe <schwabejack04@gmail.com>
2024-09-30 22:58:45 -0400

    Updating to most recent version. including the message that displays if the user has won or guessed correctly, and init a char array for the user to pick letters from

--- Words_without_friends_jack_Schwabe.c
+++ Words_without_friends_jack_Schwabe.c
@@ -5,6 +5,9 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+char validLetters[] = "ADEFNORRSSW";
+int validLettersdistro[26];
+
 int initialization();
 void gameLoop();
 void teardown();
@@ -14,99 +17,105 @@ void acceptInput();
 void make_upper(char *string);
 void strip_string(char *string);
 void getLetterDistribution(char *input, int *distro);
-bool compareCounts(int *candidate_word, int *options);
+bool compareCounts(int *candidate_word_distro, int *valid_words_distro);
 
 int main()
 {
-	initialization();
-	gameLoop();
-	teardown();
-	return 0;
+    initialization();
+    gameLoop();
+    teardown();
+    return 0;
 }
 
 int initialization()
 {
-	srand(time(NULL));
-	return 0;
+    srand(time(NULL));
+    return 0;
 }
 
 void gameLoop()
 {
-	do
-	{
-		displayWorld();
-		acceptInput();
-	}while(!isDone());
+    make_upper(validLetters);
+    getLetterDistribution(validLetters, validLettersdistro);
+    do
+    {
+        displayWorld();
+        acceptInput();
+    }while(!isDone());
 }
 
 void teardown()
 {
-	printf("All Done\n");
+    printf("All Done\n");
 }
 
 bool isDone()
 {
-	return true;
+    return true;
 }
 
 void displayWorld()
 {
-	printf("---------------------\n");
+    for(int i = 0; i < strlen(validLetters); i++) {
+        printf("%c ", validLetters[i]);
+    }
+    printf("\n---------------------\n");
 }
 
 void acceptInput()
 {
     int distro[26];
-	char buffer[20];
-	printf("Enter a guess: ");
-	fgets(buffer, sizeof(buffer), stdin);
-	make_upper(buffer);
-	strip_string(buffer);
-	getLetterDistribution(buffer, distro);
+    char buffer[20];
+    printf("Enter a guess: ");
+    fgets(buffer, sizeof(buffer), stdin);
+    make_upper(buffer);
+    strip_string(buffer);
+    getLetterDistribution(buffer, distro);
+
+    if(compareCounts(distro, validLettersdistro)) {
+        printf("You win!\n");
+
+    } else {
+        printf("You lose!\n");
+    }
 }
 
 
 void make_upper(char *string)
 {
-	int length = strlen(string);
-	for(int i = 0; i < length; i++)
-	{
-		string[i] = toupper(string[i]);
-	}
+    int length = strlen(string);
+    for(int i = 0; i < length; i++)
+    {
+        string[i] = toupper(string[i]);
+    }
 }
 
 void strip_string(char *string)
 {
-	string[strlen(string) - 1] = '\0';
+    string[strlen(string) - 1] = '\0';
 }
 
 void getLetterDistribution(char *input, int *distro)
 {
-	for(int i = 0; i < 26; i++)
-	{
-		distro[i] = 0;
-	}
-
-	for(int i = 0; i < strlen(input); i++)
-	{
-		int index = input[i] - 'A';
-		distro[index]++;
-	}
-
-	for(int k = 0; k < 26; k++)
-	{
-		printf("the distro at index: %d is %d\n", k , distro[k]);
-	}
+    for(int i = 0; i < 26; i++)
+    {
+        distro[i] = 0;
+    }
+    for(int i = 0; i < strlen(input); i++)
+    {
+        int index = input[i] - 'A';
+        distro[index]++;
+    }
 }
 
 bool compareCounts(int *candidate_word_distro, int *valid_words_distro)
 {
-	for(int i = 0; i < 26; i++)
-	{
-		if(candidate_word_distro[i] > valid_words_distro[i])
-		{
-			return false;
-		}
-	}
-	return true;
+    for(int i = 0; i < 26; i++)
+    {
+        if(candidate_word_distro[i] > valid_words_distro[i])
+        {
+            return false;
+        }
+    }
+    return true;
 }
